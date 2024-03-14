6b33d23cecdb2913197fcf2f23db0feb381f7492
avlo2000 <pmykalov@gmail.com>
2024-03-14 09:50:24 +0200

    guessing

--- probability_theory/guessing.py
+++ probability_theory/guessing.py
@@ -3,7 +3,7 @@ import copy
 import numpy as np
 np.random.seed(0)
 
-N = 8
+N = 16
 gt = np.random.randint(0, 2, N).astype(bool)
 
 
@@ -11,32 +11,40 @@ def get_score(g) -> int:
     return np.sum(g == gt).item()
 
 
-print(gt)
+for i in gt:
+    print(1 if i else 0, end='')
+print()
 initial = np.zeros(N).astype(bool)
-print(get_score(initial))
-initial[:N//2] = True
-print(get_score(initial))
+cnt_zeros = get_score(initial)
+cnt_ones = len(initial) - cnt_zeros
 
 
-def go(left: int, right: int, space: int, guess: np.ndarray, prev_score: int, zeros: bool):
+def go(left: int, right: int, space: int, guess: np.ndarray, prev_score: int, d: int):
     if left == right:
         return
-    to_print = f'[{left}, {right}] ---> ' if left + 1 != right else f'[{left}, {right}]'
+    to_print = f'[{left}, {right}] -> ' if left + 1 != right else f'[{left}, {right}]'
     score = get_score(guess)
-    to_print = f"score: {score}, diff: {score - prev_score} " + to_print
+    k = score - prev_score
+    to_print = f"score: {score}, " + to_print
+
+    n = right - left
+    true = (n + k) // 2
+    false = n - true
+    to_print = f"'t': {false} | {to_print}"
+    to_print = f"'f': {true} | {to_print}"
     print(to_print, end='')
     mid = (right - left + 1) // 2
 
     guess_l = copy.deepcopy(guess)
     guess_l[mid:] = ~guess_l[mid:]
-    go(left + mid, right, space + len(to_print), guess_l, score, ~zeros)
+    go(left + mid, right, space + len(to_print), guess_l, score, d + 1)
     print()
     print(' ' * (space + len(to_print)), end='')
 
     guess_r = copy.deepcopy(guess)
     guess_r[:mid] = ~guess_r[:mid]
-    go(left, right - mid, space + len(to_print), guess_r, score, ~zeros)
+    go(left, right - mid, space + len(to_print), guess_r, score, d + 1)
 
 
 guess = np.zeros(N).astype(bool)
-go(0, N, 0, guess, get_score(guess), False)
+go(0, N, 0, guess, get_score(guess), 0)
